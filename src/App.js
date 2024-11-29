1d656702349078ed396933a2ae6993d899197862
ydzly <ydglitch@gmail.com>
2024-11-29 10:29:54 +0000

    upd: app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import MovieCard from "./Components/MovieCard";
+import { MovieCard } from "./Components/MovieCard";
 import "./App.css";
+import { useEffect, useState } from "react";
+import { getMovies } from "./Helpers/helpers";
+import Catalog from "./Components/catalog";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    async function movieReq(params) {
+      const movieList = await getMovies();
+      setMovies(movieList["results"]);
+      console.log(movieList, "got it");
+    }
+    movieReq();
+  }, []);
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <MovieCard />
+      <Catalog movieList={movies}/>
     </div>
   );
 }
