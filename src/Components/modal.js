7cfc53e005de7de6c816c0ee431d52f835c9628d
ydzly <ydglitch@gmail.com>
2024-11-30 09:44:36 +0000

    upd: ui

--- src/Components/modal.js
+++ src/Components/modal.js
@@ -4,13 +4,14 @@ import Modal from "@mui/material/Modal";
 
 const style = {
   position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
+  top: "40%",
+  left: "40%",
+  transform: "translate(-40%, -40%)",
   bgcolor: "background.paper",
+  overflow: "scroll",
   border: "1px solid #000",
   boxShadow: 24,
-  p: 4,
+  p:1,
 };
 
 export default function MyModal(props) {
