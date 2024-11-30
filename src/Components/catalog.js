179962d9cbba6fe6e2893ecb4eec0523e4b72cd7
ydzly <ydglitch@gmail.com>
2024-11-30 09:44:04 +0000

    upd: add modal + ui tweaks

--- src/Components/catalog.js
+++ src/Components/catalog.js
@@ -5,24 +5,27 @@ import { Detail } from "./Detail";
 
 export default function Catalog(props) {
   const [open, setOpen] = React.useState(false);
+  const [openedMovieInd, setOpenedMovieInd] = React.useState({})
   const handleClose = () => setOpen(false);
 
   return (
-    <div className="container mx-auto m-3 p-3 md:p-0 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-10  ">
-      {props.movieList.map((movie) => {
+    <div className="container mx-auto p-3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-10  ">
+      {props.movieList.map((movie, ind) => {
         return (
           <div
             key={Math.random()}
             className="transform duration-500 hover:-translate-y-2 hover:shadow-xl cursor-pointer"
           >
             {" "}
-            <button onClick={() => setOpen(true)}>
-              <MovieCard />
+            <button onClick={() => {setOpen(true)
+             setOpenedMovieInd(ind)}
+          }>
+              <MovieCard movie={movie} num={ind+1}/>
             </button>
           </div>
         );
       })}
-      <MyModal open={open} handleClose={handleClose} comp={() => <Detail />} />
+      <MyModal open={open} handleClose={handleClose} comp={() => <Detail movie={props.movieList[openedMovieInd]} handleClose={handleClose}/>} />
     </div>
   );
 }
