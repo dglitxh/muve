1052288e59d0ab1d219a4906931fe1295aede01b
ydzly <ydglitch@gmail.com>
2024-11-30 07:48:28 +0000

    upd: cat ui

--- src/Components/catalog.js
+++ src/Components/catalog.js
@@ -1,23 +1,28 @@
 import { React } from "react";
 import { MovieCard } from "./MovieCard";
+import MyModal from "./modal";
+import { Detail } from "./Detail";
 
 export default function Catalog(props) {
+   const [open, setOpen] = React.useState(false);
+   const handleClose = () => setOpen(false);
+
   return (
-    <div className="container m-3 p-3 md:p-0 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-10 items-start ">
+    <div className="container mx-auto m-3 p-3 md:p-0 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-10  ">
       {props.movieList.map((movie) => {
         return (
-          <div key={Math.random()}>
+          <div
+            key={Math.random()}
+            className="transform duration-500 hover:-translate-y-2 hover:shadow-xl cursor-pointer"
+          >
             {" "}
-            <button
-              data-modal-target="default-modal"
-              data-modal-toggle="default-modal"
-              type="button"
-            >
-              <MovieCard />
+            <button onClick={setOpen(true)}>
+              <MovieCard open={open} handleClose={handleClose} comp={<Detail/>} />
             </button>
           </div>
         );
       })}
+      <MyModal/>
     </div>
   );
 }
